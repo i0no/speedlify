7f68fa59efabccbf13bd0a969c6b8e483aa6a1fd
Ion Orion <95670758+i0no@users.noreply.github.com>
2022-12-18 20:06:17 +0700

    Update iono.js

--- _data/sites/iono.js
+++ _data/sites/iono.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "iono.me", // optional, falls back to object key
-	description: "The default sites that get tested",
+	description: "iono.me | notes  of a knowledge seeker",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
